462fbcfdd971ae08be812da2b339e69331108e2f
rushan <rushanarunod@gmail.com>
2025-04-20 11:28:25 +0800

    Initial commit

---
+++ src/main/java/org/survey/example/App.java
@@ -0,0 +1,13 @@
+package org.survey.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
